447d11a07cde00ce8c980ab9e46ebb3eeae90e13
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-02-14 01:35:02 +0430

    integration test 100%

--- server_integration_test.go
+++ server_integration_test.go
@@ -17,16 +17,33 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	user2 := "Mehmet"
 	user3 := "Davut"
 
-	// Calls PUT method three times
+	// Creates
 	server.ServeHTTP(httptest.NewRecorder(), newPutWalletRequest(user1))
 	server.ServeHTTP(httptest.NewRecorder(), newPutWalletRequest(user2))
+
+	// Tries to create the same user twice
+	server.ServeHTTP(httptest.NewRecorder(), newPutWalletRequest(user3))
 	server.ServeHTTP(httptest.NewRecorder(), newPutWalletRequest(user3))
 
-	response := httptest.NewRecorder() // spy
-	server.ServeHTTP(response, newGetWalletsRequest("")) // server
+	// Tries to create a user with empty string
+	server.ServeHTTP(httptest.NewRecorder(), newPutWalletRequest(""))
+
+	// Calls PUT method three times
+	server.ServeHTTP(httptest.NewRecorder(), newPostWalletRequest(user1, 420))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWalletRequest(user2, -200))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWalletRequest("Habib", 340))
+
+	// Calls Get method two times
+	server.ServeHTTP(httptest.NewRecorder(), newGetWalletsRequest(user1))
+	server.ServeHTTP(httptest.NewRecorder(), newGetWalletsRequest("Unknown User")) // Calls GET with unknown user
+
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, newGetWalletsRequest("")) // Calls Get method
 	assertStatus(t, response.Code, http.StatusOK) // asserts status code
 
-	want := fmt.Sprintf("{\"%s\":%d,\"%s\":%d,\"%s\":%d}", user3, 0, user2, 0, user1, 0)
+	// expected response
+	want := fmt.Sprintf("{\"%s\":%v,\"%s\":%d,\"%s\":%d}",
+		user3, store.store[user3], user2, store.store[user2], user1, store.store[user1])
 
 	assertResponseBody(t, response.Body.String(), want) // asserts response body
 }
