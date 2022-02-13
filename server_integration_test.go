b5573ac0003d6c7c10e33c0d7791f5d46e77d197
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-02-14 00:42:33 +0430

    integration test: comment added

--- server_integration_test.go
+++ server_integration_test.go
@@ -8,22 +8,25 @@ import (
 )
 
 func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+	// Created new store and service
 	store := NewInMemoryWallet()
 	server := WalletServer{store}
 
+	// Created three users
 	user1 := "Recep"
 	user2 := "Mehmet"
 	user3 := "Davut"
 
+	// Calls PUT method three times
 	server.ServeHTTP(httptest.NewRecorder(), newPutWalletRequest(user1))
 	server.ServeHTTP(httptest.NewRecorder(), newPutWalletRequest(user2))
 	server.ServeHTTP(httptest.NewRecorder(), newPutWalletRequest(user3))
 
-	response := httptest.NewRecorder()
-	server.ServeHTTP(response, newGetWalletsRequest(""))
-	assertStatus(t, response.Code, http.StatusOK)
+	response := httptest.NewRecorder() // spy
+	server.ServeHTTP(response, newGetWalletsRequest("")) // server
+	assertStatus(t, response.Code, http.StatusOK) // asserts status code
 
 	want := fmt.Sprintf("{\"%s\":%d,\"%s\":%d,\"%s\":%d}", user3, 0, user2, 0, user1, 0)
 
-	assertResponseBody(t, response.Body.String(), want)
+	assertResponseBody(t, response.Body.String(), want) // asserts response body
 }
